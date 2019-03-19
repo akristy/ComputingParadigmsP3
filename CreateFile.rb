38060217231f86271e1d23924f55bdcb5c56d84e
akristy <alexanderkristy@aol.com>
2019-03-18 22:00:33 -0500

    Finished CreatFileClass
    Put a test in main and finished what I think will work with my CreateFile class but I wanted to commit to GitHub before I tested it.

--- CreateFile.rb
+++ CreateFile.rb
@@ -1,12 +1,32 @@
 class CreateFile
+    String FileName
+    String FilePath
+
     def initialize()
         puts "I am in the CreateFileClass and I am about to create a file."
     end
     #Not sure what to put as the parameters. I know that it needs to take a file name and a path
-    def CreateFileCommand(path, FileName)
-
+    #This creates the command
+    def CreateFileCommand(fpath, fname)
+        @Filename = fname
+        @FilePath = fpath
     end
 
+    #Gives the description of the command and the file path
+    def description()
+        #out puts what the command is doing and the path it is taking to do it
+        puts "Creating a file: #{FilePath}"
+    end
 
+    #execute the Create File class
+    def execute()
+        File.new(FileName)
+    end
+
+    #Since the undo function is supposed to do the inverse of the command the inverse of creating is deleting 
+    #so I delete the file that was created
+    def undo()
+        File.delete(FileName) if File.exist?(FileName)
+    end
 
 end
--- main.rb
+++ main.rb
@@ -3,3 +3,5 @@ require_relative 'CreateFile'
 test1 = CreateFile.new
 test2 = CreateFile.new
 test3 = CreateFile.new
+
+test4 = CreateFileCommand.new("C:\Users\Alexa_000\Desktop\P3 Practice","Foo")
