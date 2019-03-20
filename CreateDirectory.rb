ff4963047fdcf212cdf809566adc4124cd2aaa35
akristy <alexanderkristy@aol.com>
2019-03-19 22:58:28 -0500

    Started CreateDirectory class
    Started to write the CreateDirectory class but I am unsure how to actually make a directory. I am a bit confused on the syntax.

--- CreateDirectory.rb
+++ CreateDirectory.rb
@@ -1,3 +1,29 @@
 class CreateDirectory
+    String DirectoryName
+    
+    def initialize()
+        puts "I am in the CreateDirectoryClass and I am about to create a directory."
+    end
+    #Not sure what to put as the parameters. I know that it needs to take a file name and a path
+    #This creates the command
+    def CreateFileCommand(dirname)
+        @DirectoryName = dirname
+    end
 
+    #Gives the description of the command and the file path
+    def description()
+        #out puts what the command is doing and the path it is taking to do it
+        puts "Creating a Directory: #{DirectoryName}"
+    end
+
+    #execute the Create Directory class
+    def execute()
+        
+    end
+
+    #Since the undo function is supposed to do the inverse of the command the inverse of creating is deleting 
+    #so I delete the directory that was created
+    def undo()
+        
+    end
 end
