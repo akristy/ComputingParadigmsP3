6f3f8df284bb060ea274b00669a7784fe538c7a3
akristy <alexanderkristy@aol.com>
2019-03-19 23:34:31 -0500

    Started MoveDirectoryClass
    Started to write the MoveDirectory class but I am unsure on the syntax and did not finish the execute or undo methods

--- MoveDirectory.rb
+++ MoveDirectory.rb
@@ -1,3 +1,28 @@
 class MoveDirectory
+    String DirectoryName
 
+    def initialize()
+        puts "I am in the MoveDirectoryClass and I am about to move a directory."
+    end
+    #Not sure what to put as the parameters. I know that it needs to take a file name and a path
+    #This creates the command
+    def MoveDirectoryCommand(dirname)
+        @DirectoryName = dirname
+    end
+
+    #Gives the description of the command and the file path
+    def description()
+        #out puts what the command is doing and the path it is taking to do it
+        puts "Moving a Directory: #{DirectoryName}"
+    end
+
+    #execute the MoveDirectory class
+    def execute()
+        
+    end
+
+    #Since the undo function is supposed to do the inverse of the command the inverse of moving is moving again
+    def undo()
+        
+    end
 end
