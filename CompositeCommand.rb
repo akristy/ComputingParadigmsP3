8cdbf7120611442a112255e6c29dd5552b18053d
akristy <alexanderkristy@aol.com>
2019-03-20 21:08:16 -0500

    Started CompositeCommand class

---
+++ CompositeCommand.rb
@@ -0,0 +1,24 @@
+class CompositeCommand
+
+    def initialize(dirpath)
+        #puts "I am in the CreateDirectoryClass and I am about to create a directory."
+        
+    end
+
+    #Gives the description of the command and the file path
+    def description()
+        #out puts what the command is doing and the path it is taking to do it
+        #puts "Creating a Directory: #{@DirectoryPath}"
+    end
+
+    #execute the Create Directory class
+    def execute()
+        
+    end
+
+    #Since the undo function is supposed to do the inverse of the command the inverse of creating is deleting 
+    #so I delete the directory that was created
+    def undo()
+        
+    end
+end
